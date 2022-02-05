c4815eb80d9e342afc3b28c9ef0377ad0e0f3a5a
Yasmin Frade <91233507+yasminfrade@users.noreply.github.com>
2022-02-05 12:17:34 -0300

    ex11
    Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.

---
+++ ex11.py
@@ -0,0 +1,6 @@
+a = float(input('Qual a altura em metros: '))
+l = float(input('Qual a largura em metros: '))
+mq = a * l
+tinta = mq / 2
+print(f'A área total é de {mq:.2f} m²')
+print(f'Será necessário {tinta:.2f} litros de tinta.')
