488f250134375153302c24602fc9ff23682ab3d3
Yasmin Frade <91233507+yasminfrade@users.noreply.github.com>
2022-02-05 12:27:12 -0300

    ex16
    Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.

---
+++ ex16.py
@@ -0,0 +1,7 @@
+num = float(input("digite um numero: "))
+print(f'O valor digitado foi {num} e sua parte inteira é {int(num)}')
+
+import math
+num = float(input("digite um numero: "))
+print(f'O valor digitado foi {num} e sua parte inteira é {math.floor(num)}')
+print(f'O valor digitado foi {num} e sua parte inteira é {math.trunc(num)}')
