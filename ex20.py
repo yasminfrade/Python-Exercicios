4f82ecc1d45021b8b165247e625344199a5546a8
Yasmin Frade <91233507+yasminfrade@users.noreply.github.com>
2022-02-05 12:41:56 -0300

    ex20
    O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.

---
+++ ex20.py
@@ -0,0 +1,8 @@
+from random import shuffle
+n1 = str(input('Aluno 1: '))
+n2 = str(input('Aluno 2: '))
+n3 = str(input('Aluno 3: '))
+n4 = str(input('Aluno 4: '))
+lista = [n1, n2, n3, n4]
+shuffle(lista)
+print(lista)
